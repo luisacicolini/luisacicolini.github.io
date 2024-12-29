5102748807dae07577c5851784c6dc873470f565
luisacicolini <lc985@cam.ac.uk>
2024-12-29 22:37:28 +0100

    chore: she lives

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://luisacicolini.github.io',
     base: 'luisacicolini.github.io',
+    integrations: [mdx()],
+
 });
---
+++ src/content/lately-up-to.md
@@ -0,0 +1,7 @@
+---
+title: "Lately up to"
+---
+
+# Lately up to: 
+
+- **[Nov. 24]** I recently went back to painting and embarked on a quest to paint poetry I like.
