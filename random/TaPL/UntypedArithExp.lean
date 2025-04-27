1863fb3bc749afa2c54cb3cc2e9d8c19f61f9781
luisacicolini <lc985@cam.ac.uk>
2025-04-27 15:31:37 +0100

    chore: stuff

--- random/TaPL/UntypedArithExp.lean
+++ random/TaPL/UntypedArithExp.lean
@@ -95,7 +95,7 @@ inductive t'.EvaluatesTo : t' → t' → Prop
 | -- ite false t₂ t₃ → t₃
   EvaluatesToFalse: t'.EvaluatesTo (t'.False.ite t₂ t₃) t₃
 | -- (c → c') → (ite c l r → ite c' l r)
-  EvaluatesToIf (h : t'.EvaluatesTo c c') : (c.ite l r).EvaluatesTo (c.ite l r)
+  EvaluatesToIf (h : t'.EvaluatesTo c c') : (c.ite l r).EvaluatesTo (c'.ite l r)
 | -- (t₁ → t₁') → (succ t₁ → succ t₂)
   EvaluatesToSucc (h : t'.EvaluatesTo t₁ t₁') : (t'.succ t₁).EvaluatesTo (t'.succ t₁')
 | -- pred 0 = 0
@@ -146,18 +146,17 @@ theorem OneStepDeterminacy' (a b c : t') (hab : t'.EvaluatesTo a b) (hac : t'.Ev
       case EvaluatesToIf a' hFalseEvalToa' =>
         cases hFalseEvalToa'
   · -- a [EvaluatesToIf]→ b
-    -- i.e. a [EvaluatesToIf] → b
+    -- i.e. a [EvaluatesToIf]→ b
     -- in this case, we know that b will be something like b = ite b1 a2 a3
-    -- and given a = ite a1 a2 a3, to have a [EvaluatesToIf] → b we need a1 → b1
-    case EvaluatesToIf a1 b1 a2 a3 haEvalTob haEvalTocImp =>
+    -- and given a = ite a1 a2 a3, to have a [EvaluatesToIf]→ b we need a1 → b1
+    case EvaluatesToIf a1 b1 a2 a3 haEvalTob ih =>
     -- haEvalTocImp : ∀ (c : t'), a1.EvaluatesTo c → b1 = c is the inductive hypothesis
     -- note that we are also supposing that a1 [EvaluatesToIf]→ b1
     intros c hIteEval
     -- we study the reductions that apply to hIteEval
     -- under the hypotheses that
-    -- (1) (a1 → c1) → b1 = c1
+    -- (1) (a1 → c) → b1 = c
     -- (2) a1 → b1
-    -- given the hypothesis
     cases hIteEval
     · -- we suppose a1 [EvaluatesToTrue]→ c
       -- from (2) it follows that true → b, which is absurd
@@ -166,21 +165,108 @@ theorem OneStepDeterminacy' (a b c : t') (hab : t'.EvaluatesTo a b) (hac : t'.Ev
       case EvaluatesToFalse => cases haEvalTob
     · -- we suppose a1 [EvaluatesToIf]→ c
       -- from (2) we know a1 → b1 : the evaluation rule applied to a must be the same
-      case EvaluatesToIf haEvalToc1 => rfl
+      -- considering the inductive hypothesis, if we can prove a1 → c then we're done.
+      case EvaluatesToIf c1 haEvalToc =>
+        rw [ih]
+        apply haEvalToc
+  · -- a [EvaluatesToSucc]→ b
+    case EvaluatesToSucc a1 b1 haEvalTob ih  =>
+    intro c haSuccEvalToc
+    -- again we consider the rules for which haSuccEvalToc could happen
+    -- which is only one: a1.succ [EvaluatesToSucc]→ c
+    cases haSuccEvalToc
+    · case EvaluatesToSucc c haEvalToc =>
+      rw [ih]
+      apply haEvalToc
+  · -- a [EvaluateToZero]→ b
+    case EvaluatesToZero =>
+    -- if this is the case, we have a = t'.pred t'.zero as per the rule
+    intro c hzeroEvalTo
+    cases hzeroEvalTo
+    · case EvaluatesToZero => rfl
+    · case EvaluatesToPred c hZeroEvalTo =>
+        cases c
+        · case True => cases hZeroEvalTo -- absurd
+        · case False => cases hZeroEvalTo -- absurd
+        · case ite =>  cases hZeroEvalTo -- absurd
+        · case zero => cases hZeroEvalTo -- absurd
+        · case succ s => cases hZeroEvalTo -- absurd
+        · case pred s => cases hZeroEvalTo -- actual predecessor
+        · case iszero => cases hZeroEvalTo -- trivial
+  · -- a [EvaluatesToPredSucc]→ b
+    case EvaluatesToPredSucc a1 =>
+    -- we suppose a = t'.pred (t'.succ t₁)
+    intro c hSuccPredEvalTo
+    -- consider hp: pred (succ a1) → c
+    cases hSuccPredEvalTo
+    · case EvaluatesToPredSucc => rfl
+    · -- suppose pred (succ a1) [EvaluatesToPred]→ c1
+      case EvaluatesToPred c1 hSuccEvalTo =>
+        -- then a1 = pred c1
 
+        cases c1
+        · case True => cases hSuccEvalTo
+        · case False => cases hSuccEvalTo
+        · case ite => cases hSuccEvalTo
+        · case zero => cases hSuccEvalTo
+        · -- suppose a1 = pred (succ s)
 
+          case succ s =>
+            -- hp: succ a1 → succ s
+            -- goal : a1 = pred (succ s)
+            cases hSuccEvalTo
+            case EvaluatesToSucc ha1EvalTo =>
+            · cases a1
+              · case True => cases ha1EvalTo
+              · case False => cases ha1EvalTo
+              · -- succ (ite t1 t2 t3) → succ (succ s)
+                -- goal : ite t1 t2 t3 = pred (succ s)
+                case ite t1 t2 t3 =>
+                cases ha1EvalTo
+                · case EvaluatesToTrue => sorry
+                · case EvaluatesToFalse => sorry
+                · case EvaluatesToIf ht1Eval => sorry
+              · case _ => cases ha1EvalTo
+              · case _ =>
+                  sorry
+              · case _ =>
+                  sorry
+              · case _ =>
+                  sorry
+        · case pred s => cases hSuccEvalTo
+        · case iszero s => cases hSuccEvalTo
+  · -- a [EvaluatesToPred]→ b
+    case EvaluatesToPred a1 b1 haEvalTob ih =>
+    intro c hPredEvalTo
+    cases hPredEvalTo
+    · case EvaluatesToZero => cases haEvalTob
+    · case EvaluatesToPredSucc =>
+        cases haEvalTob
+        case EvaluatesToSucc c' hcEvalTo =>
 
-
-
-
-
-  · sorry
-  · sorry
-  · sorry
-  · sorry
-  · sorry
-  · sorry
-  · sorry
+          sorry
+    · case EvaluatesToPred => sorry
+  · -- a [EvaluatesToIsZeroZero]→ b
+    case EvaluatesToIsZeroZero =>
+    intro c hZeroIsZeroEvalTo
+    cases hZeroIsZeroEvalTo
+    · case EvaluatesToIsZeroZero => rfl
+    · case EvaluatesToIsZero c' hZeroEvalTo => cases hZeroEvalTo
+  · -- a [EvaluatesToIsZeroSucc]→ b
+    case EvaluatesToIsZeroSucc s=>
+    intro c hsuccIsZeroEvalTo
+    cases hsuccIsZeroEvalTo
+    · case EvaluatesToIsZeroSucc => rfl
+    sorry
+  · -- a [EvaluatesToIsZero]→ b
+    case EvaluatesToIsZero a1 b1 haEvalTob ih =>
+    intro c haIsZeroEvalTo
+    cases haIsZeroEvalTo
+    · case EvaluatesToIsZeroZero => cases haEvalTob
+    · case EvaluatesToIsZeroSucc s =>
+      sorry
+    · case EvaluatesToIsZero =>
+      sorry
 
 /-
 /-- Untyped Bools and Nats -/
